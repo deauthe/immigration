4b360f7dcf7bf0dcd8b47b7edf269ddc74b89381
deauthe <anurage66@gmail.com>
2025-01-03 17:08:54 +0530

    nodemailer configured

---
+++ src/app/api/question/email-question/route.ts
@@ -0,0 +1,48 @@
+// pages/api/send-question.ts
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: NextRequest) {
+	const body = await req.json();
+	const { question, email, phone, name } = body;
+
+	if (!question || !email) {
+		console.log("empty fields", body);
+
+		return NextResponse.json({ error: `empty fields` }, { status: 505 });
+	}
+
+	try {
+		// Configure your email transport
+		const transporter = nodemailer.createTransport({
+			service: "gmail", // e.g., "gmail"
+			port: 465,
+			secure: true,
+			auth: {
+				user: process.env.NEXT_PUBLIC_SENDER_EMAIL, // Your email address
+				pass: process.env.NEXT_PUBLIC_SENDER_EMAIL_PASSWORD, // Your email password or app-specific password
+			},
+		});
+
+		// Email content
+		const mailOptions = {
+			from: process.env.NEXT_PUBLIC_SENDER_EMAIL,
+			to: process.env.NEXT_PUBLIC_RECEIVER_EMAIL, // The email where you want to receive questions
+			subject: `New Question from ${name} : ${email}`,
+			text: `You received a new question: \n\n${question} \n\n phone number : ${phone}`,
+		};
+
+		// Send the email
+		transporter.sendMail(mailOptions, (err, response) => {
+			console.log("err", err, "response", response);
+		});
+
+		return NextResponse.json(question, { status: 200 });
+	} catch (error) {
+		console.error("Error sending email:", error);
+		return NextResponse.json(
+			{ error: `Error emailing question : ${error}` },
+			{ status: 500 }
+		);
+	}
+}
--- src/app/contact/QuestionForm.tsx
+++ src/app/contact/QuestionForm.tsx
@@ -10,220 +10,221 @@ import { useSession } from "next-auth/react";
 import * as z from "zod";
 import LoginButton from "@/components/LoginButton";
 import { QuestionFormSchema } from "@/types/questionType";
-import { IconError404 } from "@tabler/icons-react";
+import { IconError404, IconFaceIdError } from "@tabler/icons-react";
 import { send } from "process";
 type Question = z.infer<typeof QuestionFormSchema>;
 
 const QuestionFormContent = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-    setValue,
-    clearErrors,
-  } = useForm<Question>({
-    resolver: zodResolver(QuestionFormSchema),
-  });
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+		setValue,
+		clearErrors,
+	} = useForm<Question>({
+		resolver: zodResolver(QuestionFormSchema),
+	});
 
-  const [email, setEmail] = useState<string>();
-  const [isLoading, setIsloading] = useState(false);
+	const [email, setEmail] = useState<string>();
+	const [isLoading, setIsloading] = useState(false);
 
-  const router = useRouter();
-  const { toast } = useToast();
-  const session = useSession();
-  const [status, setStatus] = useState<string | null>(null);
+	const router = useRouter();
+	const { toast } = useToast();
+	const session = useSession();
+	const [status, setStatus] = useState<string | null>(null);
 
-  useEffect(() => {
-    const email = session.data?.user?.email;
-    if (email) {
-      setEmail(email);
-      setValue("email", email);
-    }
-  }, [session, setValue]);
+	useEffect(() => {
+		const email = session.data?.user?.email;
+		if (email) {
+			setEmail(email);
+			setValue("email", email);
+		}
+	}, [session, setValue]);
 
-  useEffect(() => {
-    console.log("errors hai:", errors);
-  }, [errors]);
+	const onSubmit: SubmitHandler<Question> = (data: Question) => {
+		setIsloading(true);
+		const parseSuccess = QuestionFormSchema.safeParse(data);
+		console.log("user data:", data, "parseSuccess:", parseSuccess);
+		//parsing error handle
+		if (!parseSuccess.success) {
+			console.log("error in parsing data", parseSuccess.error);
+			toast({
+				title: "error",
+				description: `your response submission failed`,
+				action: (
+					<div className="">
+						<IconError404 />
+					</div>
+				),
 
-  const sendMailHandler: SubmitHandler<Question> = async (data: Question) => {
-    setIsloading(true);
+				duration: 5000,
+			});
+		}
 
-    try {
-      const response = await fetch('/api/email-question', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: data.email, question: data.question, name: data.name, phone: data.phone }),
-      });
+		let succesPost = false;
+		let successMail = false;
+		let name = data.name;
 
-      if (response.ok) {
-        toast({
-          title: 'Email sent successfully',
-          description: 'Your question has been sent successfully.',
-          duration: 5000,
-        })
-      } else {
-        toast({
-          title: 'Failed to send email',
-          description: 'An error occurred while sending your question. Please try again.',
-          duration: 5000,
-        })
-      }
-    } catch (error) {
-      setStatus('An error occurred. Please try again.');
-    }
-  };
+		//axios post request for database saving
+		try {
+			axios
+				.post("/api/question", data)
+				.then((res) => {
+					console.log("response", res.data);
+					succesPost = true;
+				})
+				.catch((err) => {
+					toast({
+						title: "error",
+						description: `your response submission failed`,
+						action: (
+							<div className="">
+								<IconError404 />
+							</div>
+						),
 
-  const onSubmit: SubmitHandler<Question> = (data: Question) => {
-    setIsloading(true);
-    const parseSuccess = QuestionFormSchema.safeParse(data);
-    console.log("user data:", data, "parseSuccess:", parseSuccess);
-    if (parseSuccess.success) {
-      try {
-        axios
-          .post("/api/question", data)
-          .then((res) => {
-            console.log("response", res.data);
-            toast({
-              title: "Success",
-              description: `your response has been submitted ${res.data?.name}`,
+						duration: 5000,
+					});
+					console.error(err);
+				});
 
-              duration: 5000,
-            });
-            router.push("/");
-          })
-          .catch((err) => {
-            toast({
-              title: "error",
-              description: `your response submission failed`,
-              action: (
-                <div className="">
-                  <IconError404 />
-                </div>
-              ),
+			//axios post request for email
+			axios
+				.post("/api/question/email-question", data)
+				.then((res) => {
+					console.log("response", res.data);
+					successMail = true;
+				})
+				.catch((err) => {
+					toast({
+						title: "error",
+						description: `your response could not be mailed ${err}`,
+						action: (
+							<div className="">
+								<IconFaceIdError />
+							</div>
+						),
 
-              duration: 5000,
-            });
-            console.error(err);
-          })
-          .finally(() => {
-            setIsloading(false);
-          });
-      } catch (err) {
-        console.log("error in submitting question", err);
-      }
-    } else {
-    }
-  };
+						duration: 5000,
+					});
+				})
+				.finally(() => {
+					setIsloading(false);
+				});
 
-  return (
-    <form
-      onSubmit={() => {
-        handleSubmit(onSubmit)
-        handleSubmit(sendMailHandler)
-      }
-      }
-      className="du-form-control   p-4 rounded-3xl bg-transparent mx-auto w-full items-center flex flex-col gap-3 text-black"
-    >
-      <div className="mx-auto w-full ">
-        <Label
-          className="du-label-text du-label w-fit  uppercase font-light"
-          htmlFor="mobileNo"
-        >
-          Email
-        </Label>
-        <input
-          id="email"
-          className="du-input du-input-bordered du-input-primary w-full  px-2 bg-transparent"
-          type="email"
-          value={email || "please login first"}
-          disabled
-        />
-        {!email && (
-          <div className="my-2">
-            <LoginButton />
-          </div>
-        )}
-        {errors.email && (
-          <p className="text-xs text-primary">
-            {JSON.stringify(errors.email.message)}
-          </p>
-        )}
-      </div>
-      <div className="mx-auto w-full ">
-        <Label
-          className="du-label-text du-label w-fit  uppercase font-light"
-          htmlFor="mobileNo"
-        >
-          Phone
-        </Label>
-        <input
-          disabled={isLoading}
-          id="firstName"
-          className="du-input du-input-bordered du-input-primary w-full  px-2 bg-transparent"
-          type="text"
-          {...register("phone", {
-            required: "phone number is required for whatsapp",
-          })}
-        />
-        {errors.phone && (
-          <p className="text-xs text-primary">
-            {JSON.stringify(errors.phone.message)}
-          </p>
-        )}
-      </div>
+			if (succesPost && successMail) {
+				toast({
+					title: "Success",
+					description: `your response has been submitted ${name}`,
 
-      <div className="mx-auto w-full ">
-        <Label
-          className="du-label-text du-label w-fit  uppercase font-light"
-          htmlFor="name"
-        >
-          Name
-        </Label>
-        <input
-          disabled={isLoading}
-          className="du-input du-input-bordered du-input-primary w-full  px-2 bg-transparent "
-          id="lastName"
-          type="text"
-          {...register("name", { required: "Last name is required" })}
-        />
-        {errors.name && (
-          <p className="text-xs text-primary">
-            {JSON.stringify(errors.name.message)}
-          </p>
-        )}
-      </div>
+					duration: 5000,
+				});
+			} else {
+				throw new Error("posting in backend or mailing resulted in error");
+			}
+		} catch (err) {
+			console.log("error in submitting question", err);
+		}
+	};
 
-      <div className="mx-auto w-full ">
-        <Label
-          className="du-label-text du-label w-fit  uppercase font-light"
-          htmlFor="question"
-        >
-          Question
-        </Label>
-        <textarea
-          className="du-textarea-lg du-textarea shrink-0 text-wrap du-textarea-primary bg-transparent h-36 w-full justify-start p-0"
-          id="comment"
-          {...register("question")}
-        />
-        {errors.question && (
-          <p className="text-xs text-primary">
-            {JSON.stringify(errors.question.message)}
-          </p>
-        )}
-      </div>
+	return (
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="du-form-control   p-4 rounded-3xl bg-transparent mx-auto w-full items-center flex flex-col gap-3 text-black"
+		>
+			<div className="mx-auto w-full ">
+				<Label
+					className="du-label-text du-label w-fit  uppercase font-light"
+					htmlFor="mobileNo"
+				>
+					Email
+				</Label>
+				<input
+					id="email"
+					className="du-input du-input-bordered du-input-primary w-full  px-2 bg-transparent"
+					type="email"
+					value={email || "please login first"}
+					disabled
+				/>
+				{!email && (
+					<div className="my-2">
+						<LoginButton />
+					</div>
+				)}
+				{errors.email && (
+					<p className="text-xs text-primary">
+						{JSON.stringify(errors.email.message)}
+					</p>
+				)}
+			</div>
+			<div className="mx-auto w-full ">
+				<Label
+					className="du-label-text du-label w-fit  uppercase font-light"
+					htmlFor="mobileNo"
+				>
+					Phone
+				</Label>
+				<input
+					disabled={isLoading}
+					id="firstName"
+					className="du-input du-input-bordered du-input-primary w-full  px-2 bg-transparent"
+					type="text"
+					{...register("phone", {
+						required: "phone number is required for whatsapp",
+					})}
+				/>
+				{errors.phone && (
+					<p className="text-xs text-primary">
+						{JSON.stringify(errors.phone.message)}
+					</p>
+				)}
+			</div>
 
-      <button className="du-btn du-btn-primary my-2" type="submit">
-        Submit
-      </button>
-      <button className="du-btn du-btn-primary my-2" type="submit" >
-        Send Mail
-      </button>
+			<div className="mx-auto w-full ">
+				<Label
+					className="du-label-text du-label w-fit  uppercase font-light"
+					htmlFor="name"
+				>
+					Name
+				</Label>
+				<input
+					disabled={isLoading}
+					className="du-input du-input-bordered du-input-primary w-full  px-2 bg-transparent "
+					id="lastName"
+					type="text"
+					{...register("name", { required: "Last name is required" })}
+				/>
+				{errors.name && (
+					<p className="text-xs text-primary">
+						{JSON.stringify(errors.name.message)}
+					</p>
+				)}
+			</div>
 
+			<div className="mx-auto w-full ">
+				<Label
+					className="du-label-text du-label w-fit  uppercase font-light"
+					htmlFor="question"
+				>
+					Question
+				</Label>
+				<textarea
+					className="du-textarea-lg du-textarea shrink-0 text-wrap du-textarea-primary bg-transparent h-36 w-full justify-start p-0"
+					id="comment"
+					{...register("question")}
+				/>
+				{errors.question && (
+					<p className="text-xs text-primary">
+						{JSON.stringify(errors.question.message)}
+					</p>
+				)}
+			</div>
 
-    </form>
-  );
+			<button className="du-btn du-btn-primary my-2" type="submit">
+				Submit
+			</button>
+		</form>
+	);
 };
 
 export default QuestionFormContent;
